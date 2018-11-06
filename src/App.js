ee423a136d35b45718ed92775b99c01538adfe21
francescopapini <francescopapini01@gmail.com>
2018-11-06 15:12:50 +0000

    renamed header to navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
 import Navbar from './components/navbarComponent'
-import Header from './components/headerComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Navbar />
       </div>
     );
   }
--- src/components/navbarComponent/index.js
+++ src/components/navbarComponent/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
-
+ 
 class Navbar extends Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">
+          Logo
+        </div>
 
-  render(){ 
-    return(
-      <div>fdsdsadsads
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Information</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </nav>
 
-      </div>
-    )
+      </header>
+    );
   }
 }
 
