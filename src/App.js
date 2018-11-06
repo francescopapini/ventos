c8906a6053e6ebb249a797a54ac87bd1055c2dd9
francescopapini <francescopapini01@gmail.com>
2018-11-06 15:09:08 +0000

    Adding list for navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
-import Navbar from '. /components/Navbar'
+import Navbar from './components/navbarComponent'
+import Header from './components/headerComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
       </div>
     );
   }
