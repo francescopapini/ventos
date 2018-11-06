f45913db3bd56dbcd314955f39810799fa3399c8
francescopapini <francescopapini01@gmail.com>
2018-11-06 15:16:10 +0000

    cleaning footer.js file

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react'
  
-class Navbar extends Component {
+class Footer extends Component {
   render() {
     return (
       <header>
-        <div className="logo">
-          Logo
-        </div>
-
-        <nav>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Information</a></li>
-            <li><a href="#">Gallery</a></li>
-            <li><a href="#">Contact Us</a></li>
-          </ul>
-        </nav>
+        <footer>
+           
+        </footer>
 
       </header>
     );
   }
 }
 
-export default Navbar;
+export default Footer;
  
