25c3145771462ad694b0a92be6ef49ca9b9ee82a
francescopapini <francescopapini01@gmail.com>
2018-11-06 15:13:53 +0000

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
 import Navbar from './components/navbarComponent'
+import Footer from './components/footerComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Footer />
+
       </div>
     );
   }
---
+++ src/components/footerComponent/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+ 
+class Navbar extends Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">
+          Logo
+        </div>
+
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Information</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </nav>
+
+      </header>
+    );
+  }
+}
+
+export default Navbar;
