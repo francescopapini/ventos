bf27b2281eebfb119d8bc565c15dbefa6a441655
francescopapini <francescopapini01@gmail.com>
2018-11-06 14:37:13 +0000

    cleaned App.js file

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Hello
-          </p>
-        </header>
+
       </div>
     );
   }
