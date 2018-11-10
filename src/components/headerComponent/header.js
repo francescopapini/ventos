3f776c91d65bdd296d14679a5d6c9dec024bbd93
francescopapini <francescopapini01@gmail.com>
2018-11-10 11:19:00 +0000

    added navbar brand class to logo

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link to="/" className="nav-brand">Logo</Link>
+        <Link to="/" className="navbar-brand">Logo</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
