6f6e68f9d906b126593fe3d1b910a2266e518dc5
francescopapini <francescopapini01@gmail.com>
2018-11-10 11:15:03 +0000

    added bootstrap responsive navbar with links

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 class Header extends Component {
   render() {
     return (
-      <header>
+        /*
+        <header
         <div className="logo">
           Logo
         </div>
-
         <nav>
           <ul>
             <li><Link to="/">Home</Link></li>
@@ -17,8 +17,32 @@ class Header extends Component {
             <li><Link to="/contact_us">Contact Us</Link></li>
           </ul>
         </nav>
+        </ header>
+        */
 
-      </header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link to="/" className="nav-brand">Logo</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link to="/" className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/information" className="nav-link">Information</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/gallery" className="nav-link">Gallery</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact_us" className="nav-link">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
     );
   }
 }
