a9e054f4efcd2bf0573b4db182eca6dd51b7c801
francescopapini <francescopapini01@gmail.com>
2018-11-16 19:11:58 +0000

    added google api map wrapper

---
+++ src/components/map.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '500px',
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: -1.2884,
+         lng: 36.8233
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'YOUR_GOOGLE_API_KEY_GOES_HERE'
+})(MapContainer);
