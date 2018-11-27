2c43005742297fbe4a80b007253da96997355e74
francescopapini <francescopapini01@gmail.com>
2018-11-27 17:20:00 +0000

    added margin 0 to select-menu

--- src/components/Mosaic/MosaicSelectMenu.js
+++ src/components/Mosaic/MosaicSelectMenu.js
@@ -14,18 +14,18 @@ class MosaicSelectMenu extends Component {
 
   render() {
     return (
-      <div className="row text-center">
-        <div className="col-lg-4">
+      <div className="row text-center select-menu">
+        <div className="col-lg-4 mosaic-select-button house-button">
           <div className="btn" onClick={ this.displayHouseImages }>
             The House
           </div>
         </div>
-        <div className="col-lg-4">
-          <div className="btn">
+        <div className="col-lg-4 mosaic-select-button rooms-button">
+          <div className="btn" onClick={ this.displayHouseImages }>
             The Rooms
           </div>
         </div>
-        <div className="col-lg-4">
+        <div className="col-lg-4 mosaic-select-button others-button">
           <div className="btn">
             Others
           </div>
