d3d9a815ca46a8f7c123b8f8e008989628148d57
francescopapini <francescopapini01@gmail.com>
2018-11-28 17:02:41 +0000

    changed font and color

--- src/components/Mosaic/MosaicSelectMenu.js
+++ src/components/Mosaic/MosaicSelectMenu.js
@@ -3,30 +3,30 @@ import React, { Component } from 'react'
 class MosaicSelectMenu extends Component {
   constructor(props) {
     super(props);
-    this.state ={ show: true }
-    this.displayHouseImages = this.displayHouseImages.bind(this);
+    this.state ={ isHidden: true }
   }
 
-  displayHouseImages = () => {
-    const { show } = this.state;
-    this.setState( { show: !show })
+  toggleHidden () {
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
   }
 
   render() {
     return (
       <div className="row text-center select-menu">
         <div className="col-lg-4 mosaic-select-button house-button">
-          <div className="btn" onClick={ this.displayHouseImages }>
+          <div className="btn" onClick={this.toggleHidden.bind(this)}>
             The House
           </div>
         </div>
         <div className="col-lg-4 mosaic-select-button rooms-button">
-          <div className="btn" onClick={ this.displayHouseImages }>
+          <div className="btn" onClick={this.toggleHidden.bind(this)}>
             The Rooms
           </div>
         </div>
         <div className="col-lg-4 mosaic-select-button others-button">
-          <div className="btn">
+          <div className="btn" onClick={this.toggleHidden.bind(this)}>
             Others
           </div>
         </div>
