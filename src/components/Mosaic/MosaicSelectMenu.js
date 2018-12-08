c02d111b2599508df20880dca054860ede5e8445
francescopapini <francescopapini01@gmail.com>
2018-12-08 12:44:22 +0000

    removed unused buttons and added onClick to div

--- src/components/Mosaic/MosaicSelectMenu.js
+++ src/components/Mosaic/MosaicSelectMenu.js
@@ -4,20 +4,14 @@ class MosaicSelectMenu extends Component {
   render() {
     return (
       <div className="row text-center select-menu">
-        <div className="col-lg-4 mosaic-select-button house-button">
-          <div className="btn" onClick={this.props.clickHouseBtn}>          
-            The House
-            </div>
+        <div className="col-lg-4 mosaic-select-button house-button py-2" onClick={this.props.clickHouseBtn}>
+          The House
         </div>
-        <div className="col-lg-4 mosaic-select-button rooms-button">
-          <div className="btn" onClick={this.props.clickRoomsBtn}>
-            The Rooms
-          </div>
+        <div className="col-lg-4 mosaic-select-button rooms-button py-2" onClick={this.props.clickRoomsBtn}>
+          The Rooms
         </div>
-        <div className="col-lg-4 mosaic-select-button others-button">
-          <div className="btn" onClick={this.props.clickOthersBtn}>
-            Others
-          </div>
+        <div className="col-lg-4 mosaic-select-button others-button py-2" onClick={this.props.clickOthersBtn}>
+          Others
         </div>
       </div>
     );
