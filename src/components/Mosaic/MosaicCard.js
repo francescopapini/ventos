24c59c08e4446a2ccd019d025b5cd6bd43449066
francescopapini <francescopapini01@gmail.com>
2018-12-11 19:04:51 +0000

    fixing some eslint issues

--- src/components/Mosaic/MosaicCard.js
+++ src/components/Mosaic/MosaicCard.js
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const MosaicCard = (props) => {
-  return (
-    <div className="card">
-      <img 
-        src={require('../../assets/images/' + props.folder_path + props.image_url)}
-        alt=""
-        className="card-img-top"
-      />
-    </div>
-  )
-}
+const MosaicCard = props => (
+  <div className="card">
+    <img
+      src={require(`../../assets/images/${props.folder_path}${props.image_url}`)}
+      alt=""
+      className="card-img-top"
+    />
+  </div>
+);
 
 export default MosaicCard;
--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -1,36 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Header extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <Link to="/" className="navbar-brand">Logo</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+const Header = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container">
+      <Link to="/" className="navbar-brand">
+        Logo
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <Link to="/" className="nav-link">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/information" className="nav-link">Information</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/gallery" className="nav-link">Gallery</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/bookings" className="nav-link">Contact Us</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  }
-}
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/information" className="nav-link">
+              Information
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/gallery" className="nav-link">
+              Gallery
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/bookings" className="nav-link">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
 
 export default Header;
