7133629765428545b1d6c99839329956ba1346fa
francescopapini <francescopapini01@gmail.com>
2018-12-11 19:23:55 +0000

    cleaned carousel page

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-class Carousel extends Component {
-  render() {
-    return (
-      <div className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={require("../assets/images/landing/landing1.jpeg")} alt="First slide" className="d-block w-100"/>
-          </div>
-          <div className="carousel-item">
-            <img src={require("../assets/images/landing/landing2.jpeg")} alt="Second slide" className="d-block w-100"/>
-          </div>
-          <div className="carousel-item">
-            <img src={require("../assets/images/landing/landing3.jpeg")} alt="Second slide" className="d-block w-100"/>
-          </div>
-          <div className="carousel-item">
-            <img src={require("../assets/images/landing/landing4.jpeg")} alt="Second slide" className="d-block w-100"/>
-          </div>
-        </div>
+const Carousel = () => (
+  <div className="carousel slide" data-ride="carousel">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img
+          src={require('../assets/images/landing/landing1.jpeg')}
+          alt="First slide"
+          className="d-block w-100"
+        />
       </div>
-    );
-  }
-}
+      <div className="carousel-item">
+        <img
+          src={require('../assets/images/landing/landing2.jpeg')}
+          alt="Second slide"
+          className="d-block w-100"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src={require('../assets/images/landing/landing3.jpeg')}
+          alt="Second slide"
+          className="d-block w-100"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src={require('../assets/images/landing/landing4.jpeg')}
+          alt="Second slide"
+          className="d-block w-100"
+        />
+      </div>
+    </div>
+  </div>
+);
 
 export default Carousel;
