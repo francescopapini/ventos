642df2934e623115af04f7075c0e00396e4802fe
francescopapini <francescopapini01@gmail.com>
2018-12-12 11:17:40 +0000

    move remote into const

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,35 +1,24 @@
 import React from 'react';
 
+const landingImage1 = require('../assets/images/landing/landing1.jpeg');
+const landingImage2 = require('../assets/images/landing/landing2.jpeg');
+const landingImage3 = require('../assets/images/landing/landing3.jpeg');
+const landingImage4 = require('../assets/images/landing/landing4.jpeg');
+
 const Carousel = () => (
   <div className="carousel slide" data-ride="carousel">
     <div className="carousel-inner">
       <div className="carousel-item active">
-        <img
-          src={require('../assets/images/landing/landing1.jpeg')}
-          alt="First slide"
-          className="d-block w-100"
-        />
+        <img src={landingImage1} alt="First slide" className="d-block w-100" />
       </div>
       <div className="carousel-item">
-        <img
-          src={require('../assets/images/landing/landing2.jpeg')}
-          alt="Second slide"
-          className="d-block w-100"
-        />
+        <img src={landingImage2} alt="Second slide" className="d-block w-100" />
       </div>
       <div className="carousel-item">
-        <img
-          src={require('../assets/images/landing/landing3.jpeg')}
-          alt="Second slide"
-          className="d-block w-100"
-        />
+        <img src={landingImage3} alt="Third slide" className="d-block w-100" />
       </div>
       <div className="carousel-item">
-        <img
-          src={require('../assets/images/landing/landing4.jpeg')}
-          alt="Second slide"
-          className="d-block w-100"
-        />
+        <img src={landingImage4} alt="Fourth slide" className="d-block w-100" />
       </div>
     </div>
   </div>
