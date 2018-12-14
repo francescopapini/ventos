9c6a23cef93d1d885e353deaed1832b97b289020
francescopapini <francescopapini01@gmail.com>
2018-12-14 16:13:30 +0000

    fixed gallery props

--- src/components/Mosaic/MosaicSelectMenu.js
+++ src/components/Mosaic/MosaicSelectMenu.js
@@ -1,22 +1,16 @@
 import React from 'react';
 
-const MosaicSelectMenu = () => (
+const MosaicSelectMenu = props => (
   <div className="row text-center select-menu">
-    <div
-      className="col-lg-4 mosaic-select-button house-button py-2"
-      onClick={this.props.clickHouseBtn}
-    >
+    <div className="col-lg-4 mosaic-select-button house-button py-2" onClick={props.clickHouseBtn}>
       The House
     </div>
-    <div
-      className="col-lg-4 mosaic-select-button rooms-button py-2"
-      onClick={this.props.clickRoomsBtn}
-    >
+    <div className="col-lg-4 mosaic-select-button rooms-button py-2" onClick={props.clickRoomsBtn}>
       The Rooms
     </div>
     <div
       className="col-lg-4 mosaic-select-button others-button py-2"
-      onClick={this.props.clickOthersBtn}
+      onClick={props.clickOthersBtn}
     >
       Others
     </div>
