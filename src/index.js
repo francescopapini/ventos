1dbfde7d890f0df74a1920553db28fabd0fb610f
francescopapini <francescopapini01@gmail.com>
2019-01-09 15:46:19 +0000

    added country flags

--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import localeEn from 'react-intl/locale-data/en';
 import localeEs from 'react-intl/locale-data/es';
+import localeIt from 'react-intl/locale-data/it';
+import localePt from 'react-intl/locale-data/pt';
 import messagesEn from './translations/en.json';
 import messagesEs from './translations/es.json';
 import messagesIt from './translations/it.json';
-import messagesBr from './translations/es.json';
+import messagesPt from './translations/pt.json';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-addLocaleData([...localeEn, ...localeEs]);
+addLocaleData([...localeEn, ...localeEs, ...localeIt, localePt]);
 
 const messages = {
   en: messagesEn,
   es: messagesEs,
   it: messagesIt,
-  br: messagesBr,
+  pt: messagesPt,
 };
 const language = navigator.language.split(/[-_]/)[0]; // language without region code
 
